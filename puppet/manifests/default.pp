a6f531726d5f80a0f591c24fda01f463774b145b
Hugo Durães <hugo.duraes@movvo.com>
2014-12-05 17:39:22 +0000

    Install PHP 5.6.3 with Phalcon extension

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -55,11 +55,18 @@ class system {
 
   $apps = [
   	'build-essential',
-  	'git',
-  	'nodejs',
+    'pkg-config',
+    'autoconf',
+    'libxml2-dev',
+    'libssl-dev',
+    'libcurl4-openssl-dev',
     'libjpeg-dev',
     'libpng12-dev',
+    'libfreetype6-dev',
+    'libmcrypt-dev',
   	'libnotify-bin',
+    'git',
+    'nodejs',
     'mongodb-org'
   ]
 
@@ -162,6 +169,116 @@ class nginx
   }
 }
 
+/**
+ * PHP Configuration
+ */
+
+class php
+{
+  $phpVersion = '5.6.3'
+  $phpVersionPath = '5.6'
+
+  exec { 'download php':
+    command => "wget -qnv http://pt2.php.net/distributions/php-${phpVersion}.tar.gz",
+    cwd => '/home/vagrant',
+    user => 'root',
+    group => 'root',
+    unless => "test -f /home/vagrant/php-${phpVersion}.tar.gz",
+  }
+
+  exec { 'unpack php':
+    command => "tar -xf php-${phpVersion}.tar.gz",
+    cwd => '/home/vagrant',
+    user => 'root',
+    group => 'root',
+    unless => "test -d /home/vagrant/php-${phpVersion}",
+    require => Exec['download php'],
+  }
+
+  exec { 'install php':
+    command => "/home/vagrant/php-${phpVersion}/configure --prefix=/data/php-${phpVersionPath} --with-curl --with-gd --with-mysqli --with-pdo-mysql --enable-mbstring --with-jpeg-dir=/usr/lib --with-zlib --with-openssl --with-mcrypt --with-freetype-dir=/usr/lib --enable-zip --enable-fpm --disable-fileinfo && make && make install",
+    user => 'root',
+    group => 'root',
+    timeout => 3600,
+    unless => "test -d /data/php-${phpVersionPath}",
+    require => Exec['unpack php'],
+  }
+
+  # link php binaries
+  exec { 'link-php-binaries':
+    command => "ln -s /data/php-${phpVersionPath}/bin/* -t .",
+    cwd => '/usr/bin',
+    unless => 'test -f /usr/bin/php',
+    require => Exec['install php'],
+  }
+
+  # link to local file php.ini
+  file { "/data/php-${phpVersionPath}/lib/php.ini":
+    ensure =>file,
+    source => "/vagrant/files/data/php-${phpVersionPath}/lib/php.ini",
+    force => true,
+    require => Exec['install php'],
+    notify => Service['php-fpm'],
+  }
+
+  # link to local file php-fpm.conf
+  file { "/data/php-${phpVersionPath}/etc/php-fpm.conf":
+    ensure => file,
+    source => "/vagrant/files/data/php-${phpVersionPath}/etc/php-fpm.conf",
+    force => true,
+    require => Exec['install php'],
+    notify => Service['php-fpm'],
+  }
+
+  # link to local folder pool.d
+  file { "/data/php-${phpVersionPath}/etc/pool.d":
+    ensure => directory,
+    source => "/vagrant/files/data/php-${phpVersionPath}/etc/pool.d",
+    force => true,
+    recurse => true,
+    require => Exec['install php'],
+    notify => Service['php-fpm'],
+  }
+
+  # link to local folder extensions
+  file { "/data/php-${phpVersionPath}/extensions":
+    ensure => directory,
+    source => "/vagrant/files/data/php-${phpVersionPath}/extensions",
+    force => true,
+    recurse => true,
+    require => Exec['install php'],
+    notify => Service['php-fpm'],
+  }
+
+  # link to local file php-fpm
+  file { '/etc/init.d/php-fpm':
+    ensure => file,
+    source => '/vagrant/files/etc/init.d/php-fpm',
+    force => true,
+    require => Exec['install php'],
+    notify => Service['php-fpm'],
+  }
+
+  # install php-fpm as a service
+  exec { 'install php-fpm as a service':
+    command => 'chmod +x /etc/init.d/php-fpm && update-rc.d php-fpm defaults',
+    require => [Exec['install php'], File['/etc/init.d/php-fpm']],
+    notify => Service['php-fpm'],
+    unless => 'service php-fpm status',
+  }
+
+  # ensure php-fpm service is up and running
+  service { 'php-fpm':
+    ensure => running,
+    enable => true,
+    hasrestart => true,
+    require => [Exec['install php'], Exec['install php-fpm as a service']],
+    restart => 'service php-fpm restart',
+    subscribe => [File["/data/php-${phpVersionPath}/lib/php.ini"], File["/data/php-${phpVersionPath}/etc/php-fpm.conf"]],
+  }
+}
+
 include prepare
 include system
 include nginx
+include php
